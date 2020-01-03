5e59b8451b8e96a2f3ba8238ecf7d0b175d0add0
Piotr Migdał <pmigdal@gmail.com>
2020-01-03 23:23:34 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,46 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button :disabled="!isLoaded" @click="handleClick">start</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Sampler } from "tone";
+import A1 from "./assets/A1.mp3";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      isLoaded: false
+    };
+  },
+  created() {
+    this.sampler = new Sampler(
+      { A1 },
+      {
+        onload: () => {
+          this.isLoaded = true;
+        }
+      }
+    ).toMaster();
+  },
+  methods: {
+    handleClick() {
+      this.sampler.triggerAttack("A1");
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;

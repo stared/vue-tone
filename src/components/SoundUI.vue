aebc330af372c375798f1536d2a1e3ff1e0d1677
Piotr Migdal <pmigdal@gmail.com>
2020-01-16 18:05:53 +0100

    added UI for sound effects

--- src/components/SoundUI.vue
+++ src/components/SoundUI.vue
@@ -1,12 +1,61 @@
 <template>
   <div class="sound-ui">
     <h3>PJ's generator for QG</h3>
-    <p v-if="!isInitialized">Loading...</p>
-    <button @click="setupSoundtrack">Setup soundtrack</button>
+    <div v-if="!isInitialized">Soundtrack not yet loaded.<br>
+      <button @click="setupSoundtrack">Setup soundtrack</button>
+    </div>
+    <div v-if="isInitialized">
+      <div class="control-block">
+        <div class="control-block-title">
+          trigger effect sound (with random pitch)
+        </div>
+        <div class="control-block-content">
+          <div
+            v-for="i in effects"
+            :key="`effect-${i}`"
+            class="control-button"
+            @click="activateEffect(i)"
+            >
+            effect {{ i }}
+          </div>
+        </div>
+      </div>
+      <div class="control-block">
+        <div class="control-block-title">
+          generative part: on/off tracks
+        </div>
+        <div class="control-block-content">
+          <div
+            v-for="(isOn, i) in tracks"
+            :key="`effect-${i}-${isOn}`"
+            class="control-button"
+            :class="{ active: isOn }"
+            @click="toggleTrack(i)">
+            track {{ i }}: {{ isOn ? 'on' : 'off' }}
+          </div>
+        </div>
+      </div>
+      <div class="control-block">
+        <div class="control-block-title">
+          volume
+        </div>
+        <div class="control-block-content">
+          <div
+            v-for="i in volumeRange"
+            :key="`volume-${i}`"
+            class="control-button control-button-volume"
+            :class="{ active: i <= volume }"
+            @click="setVolume(i)">
+          {{ i }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import range from "lodash.range";
 import Soundtrack from "../soundtrack.js";
 
 export default {
@@ -14,6 +63,10 @@ export default {
   data: function() {
     return {
       soundtrack: new Soundtrack(),
+      tracks: range(4).map(() => true),
+      effects: range(30),
+      volumeRange: range(10).map((x) => 10 * x - 70),
+      volume: 10,
     };
   },
   created() {
@@ -31,7 +84,18 @@ export default {
       this.soundtrack.setGenerativeTrackStatus(1, true);
       this.soundtrack.setGenerativeTrackStatus(2, true);
       this.soundtrack.setGenerativeTrackStatus(3, true);
-    }
+    },
+    activateEffect(i) {
+      this.soundtrack.triggerEffect(i, Math.random());
+    },
+    toggleTrack(i) {
+      this.$set(this.tracks, i, !this.tracks[i]);
+      this.soundtrack.setGenerativeTrackStatus(i, this.tracks[i]);
+    },
+    setVolume(i) {
+      this.volume = i;
+      this.soundtrack.generativePartVolume(i);
+    },
   }
 }
 </script>
@@ -41,15 +105,35 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.control-block {
+  background-color: rgb(85, 36, 148);
+  color: white;
+  padding: 10px;
+  margin: 20px;
+  cursor: default;
 }
-li {
+
+.control-button {
   display: inline-block;
-  margin: 0 10px;
+  background-color: black;
+  border-radius: 10px;
+  padding: 5px;
+  margin: 5px;
+  width: 120px;
+  cursor: pointer;
+  border-width: 2px;
+  border-style: solid;
+  border-color: black;
 }
-a {
-  color: #42b983;
+.control-button.control-button-volume {
+  width: 60px;
+}
+.control-button:hover {
+  background-color: white;
+  color: black;
+}
+
+.active {
+  border-color: rgba(255, 0, 0, 0.7);
 }
 </style>
